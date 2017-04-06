9794c2293d49f7deca0bf720b54ebae3b477d380
Zax <zax.rosenberg@gmail.com>
2017-04-06 08:23:00 -0500

    Fixed duplication on reselect dates v2

--- date_generator.py
+++ date_generator.py
@@ -1,6 +1,7 @@
 import csv
 import tkinter as tk
 
+
 class DateGenerator(tk.Frame):
     def __init__(self):
         self.root = tk.Tk()
@@ -23,10 +24,11 @@ class DateGenerator(tk.Frame):
         end.set(2017)
         tk.OptionMenu(self.root, end, *year_choices).grid(column=1, row=1)
 
-        self.root.bind('<Button-1>', lambda x=0, y=0: self.get_date_list(start.get(), end.get()))
-        self.grid()
+        #Allow pushing enter to run, but causes all previous selections to be saved
+        #self.root.bind('<Button-1>', lambda x=0, y=0: self.get_date_list(start.get(), end.get()))
+        #self.grid()
 
-        self.submit = tk.Button(self.root, text = "Generate")
+        self.submit = tk.Button(self.root, text="Generate")
         self.submit.bind('<Button-1>', lambda x=0, y=0: self.get_date_list(start.get(), end.get()))
         self.submit.grid(column=1, row=4)
 
@@ -39,12 +41,12 @@ class DateGenerator(tk.Frame):
             date_list = self.create_date_range(start_year, end_year)
 
         self.write_to_csv(start_year, end_year, date_list)
-    
+
     def count_back(self, start_year, end_year):
         start_year, end_year = end_year, start_year
         date_list = self.create_date_range(start_year, end_year)
         return date_list[::-1]
-    
+
     def create_date_range(self, start_year, end_year):
         date_list = []
         for i in range(start_year, end_year + 1):
@@ -52,17 +54,18 @@ class DateGenerator(tk.Frame):
             for j in range(2, 5):
                 date = "{0}Q{1}".format(j, str(i)[2:4])
                 date_list.append(date)
-    
+
         return date_list
-    
+
     def write_to_csv(self, start_year, end_year, date_list):  # todo handle PermissionError when file locked
         file_name = "Dates {0} to {1} quarterly.csv".format(start_year, end_year)
         with open(file_name, 'w') as csv_file:
             writer = csv.writer(csv_file, lineterminator='\n')
-            writer.writerow(date_list)  #todo figure out how to write in column
-    
+            writer.writerow(date_list)  # todo figure out how to write in column
+
     def start(self):
         self.root.mainloop()
+
 
 if __name__ == "__main__":
     DateGenerator().start()
